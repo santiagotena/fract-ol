9520d5303e8e477bde2f7a6554a953b8d3b66121
stena-he <stena-he@student.42wolfsburg.de>
2022-09-20 21:44:52 +0200

    norminette

--- src/julia.c
+++ src/julia.c
@@ -6,13 +6,13 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 22:11:38 by stena-he          #+#    #+#             */
-/*   Updated: 2022/09/19 23:36:42 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/09/20 21:33:13 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fractol.h"
 
-void	julia(t_fractol *f, t_img *img, int x, int y, double zr, double zi)
+void	julia(t_fractol *f, t_img *img, t_point *p)
 {
 	int		n;
 	double	tmp;
@@ -20,40 +20,46 @@ void	julia(t_fractol *f, t_img *img, int x, int y, double zr, double zi)
 	n = -1;
 	while (++n < MAX_ITERATIONS)
 	{
-		if ((zr * zr + zi * zi) > 4.0)
+		if ((p->r * p->r + p->i * p->i) > 4.0)
 			break ;
-		tmp = 2 * zr * zi + f->ki;
-		zr = zr * zr - zi * zi + f->kr;
-		zi = tmp;
+		tmp = 2 * p->r * p->i + f->ki;
+		p->r = p->r * p->r - p->i * p->i + f->kr;
+		p->i = tmp;
 	}
 	if (n >= 0 && n <= (MAX_ITERATIONS / 2) - 1)
-		my_mlx_pixel_put(img, x, y, 0x00003366);
+		my_mlx_pixel_put(img, p->x, p->y, 0x00003366);
 	else if (n >= MAX_ITERATIONS / 2 && n <= MAX_ITERATIONS - 1)
-		my_mlx_pixel_put(img, x, y, 0x00000000);
+		my_mlx_pixel_put(img, p->x, p->y, 0x00000000);
 	else
-		my_mlx_pixel_put(img, x, y, 0x00CC3333);
+		my_mlx_pixel_put(img, p->x, p->y, 0x00CC3333);
 }
 
 void	draw_julia(t_fractol *f, t_img *img)
 {
-	int		x;
-	int		y;
-	double	pr;
-	double	pi;
+	t_point	p;
 
 	mlx_clear_window(f->mlx, f->win);
-	y = -1;
-	while (++y < HEIGHT)
+	p.y = -1;
+	while (++p.y < HEIGHT)
 	{
-		x = -1;
-		while (++x < WIDTH)
+		p.x = -1;
+		while (++p.x < WIDTH)
 		{
-			pr = f->min_r + (double)x * (f->max_r - f->min_r) / WIDTH;
-			pi = f->min_i + (double)y * (f->max_i - f->min_i) / HEIGHT;
-			julia(f, img, x, y, pr, pi);
+			p.r = f->min_r + (double)p.x * (f->max_r - f->min_r) / WIDTH;
+			p.i = f->min_i + (double)p.y * (f->max_i - f->min_i) / HEIGHT;
+			julia(f, img, &p);
 		}
 	}
 	mlx_put_image_to_window(f->mlx, f->win, img->img, 0, 0);
+}
+
+void	assign_julia(t_fractol *f, char **argv)
+{
+	f->f_name = argv[1];
+	f->min_r = -2.0;
+	f->max_r = 1.0;
+	f->min_i = -1.5;
+	f->max_i = f->min_i + (f->max_r - f->min_r) * HEIGHT / WIDTH;
 }
 
 void	init_julia(char **argv)
@@ -64,15 +70,11 @@ void	init_julia(char **argv)
 	f.mlx = mlx_init();
 	if (f.mlx == NULL)
 		return ;
-	f.f_name = argv[1];
-	f.min_r = -2.0;
-	f.max_r = 1.0;
-	f.min_i = -1.5;
-	f.max_i = f.min_i + (f.max_r - f.min_r) * HEIGHT / WIDTH;
+	assign_julia(&f, argv);
 	if (!ft_strncmp(argv[1], "julia", 6))
 	{
 		f.kr = ft_atod(argv[2]);
-		f.ki = ft_atod(argv[3]);	
+		f.ki = ft_atod(argv[3]);
 	}
 	f.win = mlx_new_window(f.mlx, WIDTH, HEIGHT, "Fractal");
 	img.img = mlx_new_image(f.mlx, WIDTH, HEIGHT);
