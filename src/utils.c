8484431ddc296b6fd9ca14228079c9d46c67f9a2
stena-he <stena-he@student.42wolfsburg.de>
2022-09-20 21:45:20 +0200

    norminette

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: stena-he <stena-he@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 13:54:46 by stena-he          #+#    #+#             */
-/*   Updated: 2022/09/18 15:57:04 by stena-he         ###   ########.fr       */
+/*   Updated: 2022/09/20 20:41:06 by stena-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,32 @@ void	my_mlx_pixel_put(t_img *data, int x, int y, int color)
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int *)dst = color;
+}
+
+void	atod_loop(char *arr, int afterdot, float scale, float *val)
+{
+	while (*arr)
+	{
+		if (!ft_isdigit(*arr) && !(*arr == '.'))
+		{
+			ft_printf("Wait. That's illegal.\n\n");
+			print_help();
+			exit(0);
+		}
+		if (afterdot)
+		{
+			scale = scale / 10;
+			*val = *val + (*arr - '0') * scale;
+		}
+		else
+		{
+			if (*arr == '.')
+			afterdot++;
+			else
+			*val = *val * 10.0 + (*arr - '0');
+		}
+		arr++;
+	}	
 }
 
 double	ft_atod(char *arr)
@@ -36,28 +62,7 @@ double	ft_atod(char *arr)
 		arr++;
 		neg = 1;
 	}
-	while (*arr)
-	{
-		if (!ft_isdigit(*arr) && !(*arr == '.'))
-		{
-			ft_printf("Wait. That's illegal.\n\n");
-			print_help();
-			exit(0);
-		}
-		if (afterdot)
-		{
-			scale = scale / 10;
-			val = val + (*arr - '0') * scale;
-		}
-		else
-		{
-			if (*arr == '.')
-			afterdot++;
-			else
-			val = val * 10.0 + (*arr - '0');
-		}
-		arr++;
-	}
+	atod_loop(arr, afterdot, scale, &val);
 	if (neg)
 		return (-val);
 	else
